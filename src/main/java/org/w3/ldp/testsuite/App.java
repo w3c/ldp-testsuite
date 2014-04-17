6ccd372ec176f29da61b1304d8441cb3ff43cc4b
Sergio Fernández <wikier@apache.org>
2014-04-17 09:41:36 +0200

    added maven skel project

---
+++ src/main/java/org/w3/ldp/testsuite/App.java
@@ -0,0 +1,13 @@
+package org.w3.ldp.testsuite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
