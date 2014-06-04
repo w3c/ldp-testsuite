106a36c0ec671419cdfdadee93f7ca1d98164a09
Sergio Fernández <wikier@apache.org>
2014-06-04 09:38:37 +0200

    cleanup gh-pages branch

--- src/main/java/org/w3/ldp/testsuite/http/MediaTypes.java
+++
@@ -1,8 +0,0 @@
-package org.w3.ldp.testsuite.http;
-
-public interface MediaTypes {
-
-    public final static String TEXT_TURTLE = "text/turtle";
-    public final static String APPLICATION_RDF_XML = "application/rdf+xml";
-
-}
