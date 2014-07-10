98a5c218024350c4c38fb0d17fd5f943816cd69a
Samuel Padgett <spadgett@us.ibm.com>
2014-07-10 11:20:43 -0400

    don't HTML escape stacktrace twice in execution report

--- src/main/java/org/w3/ldp/testsuite/reporter/LdpHtmlReporter.java
+++ src/main/java/org/w3/ldp/testsuite/reporter/LdpHtmlReporter.java
@@ -484,7 +484,7 @@ public class LdpHtmlReporter implements IReporter {
 		html.table(class_("indented"));
 		html.tr(class_("center")).th(class_("Failed")).content("[FAILED TEST]")
 				._tr();
-		html.tr().td(class_("throw")).content(Utils.stackTrace(thrown, true)[0])._tr();
+		html.tr().td(class_("throw")).content(Utils.stackTrace(thrown, false)[0])._tr();
 
 		html._table();
 
