275d23b59ab39b40dfc9b18e0321ef2a324100fe
sspeiche <sspeiche@gmail.com>
2014-07-11 18:14:36 -0400

    Changed to README because that is what it really is

--- report/ldp-earl-report.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -v
-#earl-report -f json -o ldp.jsonld --manifest ldp-earl-manifest.ttl contrib/*.ttl 
-#earl-report -f ttl -o ldp.ttl --manifest ldp-earl-manifest.ttl contrib/*.ttl 
-#earl-report -f html --json ldp.jsonld --template ldp-template.html.haml -o ldp.html 
-earl-report -f html -o ldp.html --name "LDP" --bibRef "[[LDP]]" --template ldp-template.html.haml --manifest ldp-earl-manifest.ttl contrib/*.ttl 
